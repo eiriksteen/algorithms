b1176635e1a5b0037c236dd3872076bf0967bdce
eiriksteen <72987142+eiriksteen@users.noreply.github.com>
2021-05-11 17:34:43 +0200

    Update lowestpalindromelarger.cpp

--- lowestpalindromelarger.cpp
+++ lowestpalindromelarger.cpp
@@ -9,20 +9,23 @@
 #include "iostream"
 using namespace std;
 
-void stringExpAddition(string& num, int add){
-    if (num[add] < '9' && num[add] >= '0'){
-        ++num[add];
+void stringExpAddition(string& num, int addIndex){
+    // adds 10 ^ (len(num) - 1 - addIndex) to number num
+    // needs to be done this way since num can be too large for normal mathematical computation
+    
+    if (num[addIndex] < '9' && num[addIndex] >= '0'){
+        ++num[addIndex];
         return;
     }
 
-    else if (add == 0){
+    else if (addIndex == 0){
         num.erase(0, 1);
         num = "10" + num;
         return;
     }
 
-    num[add] = '0';
-    return stringExpAddition(num, add - 1);
+    num[addIndex] = '0';
+    return stringExpAddition(num, addIndex - 1);
 }
 
 
@@ -35,16 +38,20 @@ string lpl(string snum) {
         char l = snum[start];
         char r = snum[end];
 
+        // continue if digits already mirror each other
         if (l == r){
             ++start;
             --end;
             continue;
         }
 
+        // set right side digit equal to left side digit if l > r (otherwise the result decreases in size)
         else if (l > r){
             snum[end] = l;
         }
 
+        // inorder to keep left side digit (higher decimal place is prioritized -> result should be as small as possible), digit 
+        // to the left of right side digit needs to increase by one (apply recursively if digit already is 9) 
         else{
             stringExpAddition(snum, end - 1);
             snum[end] = snum[start];
